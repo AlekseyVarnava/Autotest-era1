bb7862888a542af0693b59d7c0e1739383d8f116
AlekseyVarnava <123564050+AlekseyVarnava@users.noreply.github.com>
2023-05-01 14:52:56 +0300

    Commit

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -4,7 +4,7 @@ from pages.elements_page import TestElementBox
 class TestElements:
     class TestElementBox:
          def test_text_box(self, driver):
-            text_box_page = TestElementBox(driver, 'http://i.lab5.era:8080/login')
-            text_box_page.open()
-            text_box_page.test_all_form()
+            all_page = TestElementBox(driver, 'http://i.lab5.era:8080/login')
+            all_page.open()
+            all_page.test_all_form()
 
