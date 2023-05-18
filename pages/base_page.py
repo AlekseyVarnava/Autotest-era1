67ea7217aad898a7c943cb2a13823d12d8c266cc
AlekseyVarnava <123564050+AlekseyVarnava@users.noreply.github.com>
2023-05-18 16:41:53 +0300

    add allure

--- pages/base_page.py
+++ pages/base_page.py
@@ -52,4 +52,19 @@ class BasePage:
         action.context_click(element)
         action.perform()
 
+    def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
+        action = AC(self.driver)
+        action.drag_and_drop_by_offset(element, x_coords, y_coords)
+        action.perform()
+
+    def action_drag_and_drop_to_element(self, what, where):
+        action = AC(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
+
+    def action_move_to_element(self, element):
+        action = AC(self.driver)
+        action.move_to_element(element)
+        action.perform()
+
 
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,10 +1,16 @@
+import allure
+
 from pages.elements_page import TestElementBox
 
-
+@allure.suite('Integracia')
 class TestElements:
+    @allure.sub_suite('All_test')
     class TestElementBox:
+         @allure.suite('Smoke_test')
          def test_text_box(self, driver):
             all_page = TestElementBox(driver, 'http://i.lab5.era:8080/login')
             all_page.open()
             all_page.test_all_form()
+
+
 
